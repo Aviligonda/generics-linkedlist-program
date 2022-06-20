883f2cf020166442b7912c013e2b764648ff37d5
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-06-20 10:15:51 +0530

    Added : Searching the element in the linked list

--- LinkedListClass.java
+++ LinkedListClass.java
@@ -6,7 +6,7 @@ public class LinkedListClass {
         functionClass.insertLast(70);
         functionClass.insertInBetween(56, 70, 30);
         functionClass.toPrint();
-        functionClass.deleteLast();
+        functionClass.findNode(30);
         functionClass.toPrint();
     }
 }
